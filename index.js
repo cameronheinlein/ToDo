d7830746221d9b2f47cb778d7987b93fba90e92f
cameronheinlein <76449880+cameronheinlein@users.noreply.github.com>
2021-04-22 12:19:40 -0400

    add styles

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ function App(){
     {
       text: 'build todo app',
       isCompleted: false,
-    }     
-  ]);
+    }        
+  ])
 
   return(
     <>
-      {todos.map((todo, i) => <div key={i}>{todo.text}</div>)}
+      {todos.map((todo, i) => (
+        <div className="todo" key={i}>{todo.text}</div>
+      ))}
     </>
   );
 }
